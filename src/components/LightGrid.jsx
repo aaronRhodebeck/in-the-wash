0eddf2bd950afe958ad6641c37cfe92e2ffa5dac
aaronRhodebeck <arhodebeck@gmail.com>
2018-05-10 14:06:19 -0500

    Add React Component and D3 to draw light grid

--- src/components/LightGrid.jsx
+++ src/components/LightGrid.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { withFauxDOM } from 'react-faux-dom';
 import { drawLightGrid } from '../d3/lightGrid';
 
+// For creation and testing purposes
+import sampleLightGrid from '../../assets/sampleLightGrid.json';
+
 class LightGrid extends React.Component {
   constructor(props) {
     super(props);
     const faux = this.props.connectFauxDOM('div', 'lightGrid');
-    drawLightGrid(faux);
+    drawLightGrid(faux, sampleLightGrid);
   }
   render() {
     return <div>{this.props.lightGrid}</div>;
--- src/d3/lightGrid.js
+++ src/d3/lightGrid.js
@@ -1,9 +1,46 @@
 import * as d3 from 'd3';
 
-export function drawLightGrid(fauxComponent) {
-  d3
+/*
+Schema for lightGridDefinition:
+  All numbers are in feet
+
+  {
+    "theaterSize": {
+      "width": 70,
+      "depth": 50
+    },
+    "bars": [
+      {
+        "start": { "x": 5, "y": 10 },
+        "end": { "x": 65, "y": 10 }
+      }
+    ]
+  }
+*/
+
+export function drawLightGrid(fauxComponent, lightGridDefinition) {
+  let svg = d3
     .select(fauxComponent)
-    .append('h3')
-    .html('Light Grid')
-    .style('color', 'white');
+    .append('svg')
+    .style('height', '100%')
+    .style('width', '100%')
+    .style('background', 'rgba(240, 240, 240, .9)');
+
+  const theaterDepth = lightGridDefinition.theaterSize.depth;
+  const theaterWidth = lightGridDefinition.theaterSize.width;
+
+  svg
+    .attr('viewBox', `0 0 ${theaterWidth} ${theaterDepth}`)
+    .attr('preserveAspectRatio', 'xMidYMin slice');
+
+  svg
+    .selectAll('g')
+    .data(lightGridDefinition.bars)
+    .enter()
+    .append('line')
+    .attr('x1', bar => bar.start.x)
+    .attr('y1', bar => bar.start.y)
+    .attr('x2', bar => bar.end.x)
+    .attr('y2', bar => bar.end.y)
+    .style('stroke', 'black');
 }
